bae8d402e167af8e700a5b776f089388c00fb7b2
bluelovers <codelovers@users.sourceforge.net>
2020-06-11 16:20:56 +0800

    feat: newLinkURL

--- lib/util.d.ts
+++ lib/util.d.ts
@@ -1,1 +1,2 @@
-export declare function newFileURL(siteID: string, novelID: string, server?: string): URL;
+export declare function newFileURL(rootKey: string, dataKey: string, server?: string): URL;
+export declare function newLinkURL(rootKey: string, dataKey: string, server?: string): URL;
--- lib/util.ts
+++ lib/util.ts
@@ -2,7 +2,12 @@ import hashSum from 'hash-sum';
 
 const server01 = `https://api-file-server.epub.now.sh/`;
 
-export function newFileURL(siteID: string, novelID: string, server = server01)
+export function newFileURL(rootKey: string, dataKey: string, server = server01)
 {
-	return new URL(`db/file/${siteID}/${hashSum(novelID)}`, server);
+	return new URL(`db/file/${rootKey}/${hashSum(dataKey)}`, server);
+}
+
+export function newLinkURL(rootKey: string, dataKey: string, server = server01)
+{
+	return new URL(`file/${rootKey}/${hashSum(dataKey)}`, server);
 }
