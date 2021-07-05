5de7cddf5910d0db1a541b170a6bbd0be407f75f
bluelovers <codelovers@users.sourceforge.net>
2021-07-05 21:52:21 +0800

    feat: update server

--- lib/util.ts
+++ lib/util.ts
@@ -1,13 +1,14 @@
 import hashSum from 'hash-sum';
 
-const server01 = `https://api-file-server.epub.now.sh/`;
+//const server01 = `https://api-file-server.epub.now.sh/`;
+const server01 = `https://api-file-server.vercel.app/`;
 
-export function newFileURL(rootKey: string, dataKey: string, server = server01)
+export function newFileURL(rootKey: string, dataKey: string | number, server = server01)
 {
 	return new URL(`db/file/${rootKey}/${hashSum(dataKey)}`, server);
 }
 
-export function newLinkURL(rootKey: string, dataKey: string, server = server01)
+export function newLinkURL(rootKey: string, dataKey: string | number, server = server01)
 {
 	return new URL(`file/${rootKey}/${hashSum(dataKey)}`, server);
 }
